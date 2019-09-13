26c0a527d20a43ce44c4034c33d4c3aa2cd9f385
David <david.shams@gmail.com>
2019-09-13 00:13:13 -0500

    webpack.config for jsx

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,32 @@ const path = require('path');
 
 module.exports = {
     "mode": "development",
-    "entry": path.resolve(__dirname, `src`, `app`);
+    "entry": path.resolve(__dirname, `src`, `app`),
+    "output": {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: `/`
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        contentBase: "./dist",
+        hot: true,
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /.jsx$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["stage-0", "react"],
+                        plugins: []
+                    }
+                }
+            }
+        ]
+    }
 }
